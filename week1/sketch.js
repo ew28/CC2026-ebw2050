902629b3d060ba661f6c37ba0b02a04f47af1cc4
Eva Weisenfeld <98043785+ew28@users.noreply.github.com>
2026-09-09 11:32:23 -0400

    Fix quote formatting in creative coding definitions

--- week1/sketch.js
+++ week1/sketch.js
@@ -6,7 +6,7 @@ const quotes = [
   { text: "\n'a process, based on exploration, iteration, reflection and discovery,\nwhere code is used as the primary medium to create a wide range of media artifacts.'", source: "Tim" }, // https://trcc.timrodenbroeker.de/faq/what-is-creative-coding/
   { text: "\n'[coding that] doesn't follow pre-defined specifications to solve problems for a user,\nbut rather aims at expressing ideas and concepts.'", source: "Raphaël de Courville" }, // https://hackmd.io/@creativecodeberlin/ryljdLxy_
   { text: "\n'rather than a one-sided audience viewing a result,\nit can draw the audience in to interact with.'", source: "Amber" }, // https://medium.com/@agc410/intro-to-creative-coding-56aa7658fd23
-  { text: "simply the art of creating visualizations or expression.'", source: "Donovan Alexander" }, // https://interestingengineering.com/culture/everything-you-need-to-know-about-the-artistic-world-of-creative-coding
+  { text: "'simply the art of creating visualizations or expression.'", source: "Donovan Alexander" }, // https://interestingengineering.com/culture/everything-you-need-to-know-about-the-artistic-world-of-creative-coding
   { text: "\n'an exciting blend of art and technology where you use programming\nto create stunning visuals, animations, sounds, and interactive experiences.'", source: "Cam Velasco" }, // https://floowitalent.com/tips/what-is-creative-coding
   { text: "\n'the practice of using computer programming\nas a medium for artistic and personal expression.'", source: "Laura Valtere" }, // https://diatomenterprises.com/blog/what-is-creative-coding/
   { text: "'a practice where programming is used as a tool for art or design'", source: "Jack B. Du" } // https://digitalfutures.international/creative-coding/
